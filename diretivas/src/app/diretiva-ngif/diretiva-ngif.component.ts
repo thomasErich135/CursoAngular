e6b23daace8ff8fcb225f3e5e4779bac389facd9
thomasErich135 <thomas@tptecnologia.com>
2019-12-03 17:27:29 -0300

    Incluido 4 projeto - diretivas

---
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css']
+})
+export class DiretivaNgifComponent implements OnInit {
+
+  cursos: string[] = ['Angular 2'];
+  mostrarCursos: boolean = false;
+
+  onMonstrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
