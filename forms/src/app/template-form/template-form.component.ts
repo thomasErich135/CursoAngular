5dda1038bca1e60db0895866ed18e9ce6be9df56
thomasErich135 <thomas@tptecnologia.com>
2020-01-17 16:36:53 -0300

    Curso Angular

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -7,17 +10,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
-  private usuario: any = {
+  usuario: any = {
     nome: '',
-    email: ''
+    email: '',
+    endereco: {
+      cep: '',
+      numero: '',
+      complemento: '',
+      rua: '',
+      bairro: '',
+      cidade: '',
+      uf: ''
+    }
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   onSubmit(form){    
     console.log(form);
+  }
+  
+  verificaValidTouched(campo) {
+    return campo.valid && campo.touched;
+  }
+
+  verificaInvalidTouched(campo) {
+    return campo.invalid && campo.touched;
+  }
+
+  aplicaCssErro(campo) {
+    return {
+      'is-invalid': this.verificaInvalidTouched(campo),
+      'is-valid': this.verificaValidTouched(campo)
+   };
+  }
+
+  consultaCEP(cep, form) {
+      //Nova variável "cep" somente com dígitos.
+      cep = cep.replace(/\D/g, '');
+      //Verifica se campo cep possui valor informado.
+      if (cep != "") {
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+        //Valida o formato do CEP.
+        if(validacep.test(cep)) {
+                    
+          this.resetaDadosFormulario(form);
+
+          this.http.get(`//viacep.com.br/ws/${cep}/json`)
+            .subscribe(dados => this.populaDadosForm(dados, form));
+
+
+        }
+      }
+  }
+
+  populaDadosForm(dados, formulario) {
+    //utilizando o setvalue somos obrigados a definir todos os campos, melhor utilizar o pacthvalue
+    // form.setValue({
+    //   nome: form.value.nome,
+    //   email: form.value.email,
+    //   endereco: {
+    //     cep: dados.cep,
+    //     numero: form.value.endereco.numero,
+    //     complemento: dados.complemento,
+    //     rua: dados.logradouro,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     uf: dados.uf
+    //   }
+    // });
+
+    formulario.form.patchValue({
+      endereco: {
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        uf: dados.uf
+      }
+    });
+  }
+
+  resetaDadosFormulario(formulario)  {
+    formulario.form.patchValue({
+      endereco: {
+        complemento: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        uf: null        
+      }
+    });
   }
 }
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { TemplateFormComponent } from './template-form.component';
 
 import { FormDebugComponent } from './../form-debug/form-debug.component';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { FormDebugComponent } from './../form-debug/form-debug.component';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class TemplateFormModule { }
