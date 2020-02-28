d9ce9ef3034d3b6798fde8806e301c23c908f9d0
Hel <Hel@BRAYTON.LOC>
2020-02-28 16:33:34 -0300

    alteração css para scss

--- request-http/src/app/shared/alert-modal/alert-modal.component.ts
+++ request-http/src/app/shared/alert-modal/alert-modal.component.ts
@@ -5,7 +5,7 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-alert-modal',
   templateUrl: './alert-modal.component.html',
-  styleUrls: ['./alert-modal.component.css']
+  styleUrls: ['./alert-modal.component.scss']
 })
 export class AlertModalComponent implements OnInit {
 
