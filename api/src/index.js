ba7784e680469155bfe69fa13cac2ae9a5568896
thomasErich135 <thomas@tptecnologia.com>
2020-03-30 11:33:56 -0300

    delete pasta api, escolhemos trabalhar com express

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,7 @@ const restify = require('restify');
 const Router = require('restify-router').Router;
 const router = new Router();
 
+const serverPort = 8000;
 const server = restify.createServer({
     name: 'OneeWebAPI',
     version: 'v1'
@@ -11,19 +12,17 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-const serverPort = 8000;
-
 server.get('/', (req, res) => {
     res.json('Server Running - Ok');
 });
 
 //server.use(`/${server.version}`, require('./router.js'));
-router.add(`/v1`, require('./router.js'));
+//router.add(`/v1`, require('./router.js'));
 
 server.get('*', (req, res) => {
     res.json({
         status: '404 - Not Found :/',
-        msg: 'App Works :D'
+        msg: 'API Works :D'
     });
 });
 
